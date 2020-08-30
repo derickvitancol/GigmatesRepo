1b9e4e3cffd5cf700744121250a2d20a5b5c6c6c
NICHOLLE KIM FONTANILLA <nktfontanilla@mymail.mapua.edu.ph>
2020-08-30 08:21:23 -0700

    Added Signup function

--- GigmatesWCF/IService1.cs
+++ GigmatesWCF/IService1.cs
@@ -19,44 +19,108 @@ namespace GigmatesWCF
         [OperationContract]
         string GetData(int value);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
 
         [OperationContract]
         string greetings();
 
 
         [OperationContract]
-        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        //[WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
         string derick();
         // TODO: Add your service operations here
 
+        [OperationContract]
+        string Signup(SignupUser user);
+        
+        //[OperationContract]
+        //DataDB Login();
         /*
          CREATE FUNCTION FOR REGISTRATION
          LOGING IN - use an algorithm 
          */
     }
 
+    [DataContract]
+    public class SignupUser
+    {
+        bool boolVal = true;
+        string username, password, biography, location;
+        float rate;
+        int personType, age;
+
+        [DataMember]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        [DataMember]
+        public string Biography
+        {
+            get { return biography; }
+            set { biography = value; }
+        }
+
+        [DataMember]
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        [DataMember]
+        public float Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+        [DataMember]
+        public int PersonType
+        {
+            get { return personType; }
+            set { personType = value; }
+        }
+
+        [DataMember]
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+    }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
 
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
 }
--- GigmatesWCF/Service1.svc.cs
+++ GigmatesWCF/Service1.svc.cs
@@ -6,12 +6,15 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.ServiceModel.Activation;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GigmatesWCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+
     public class Service1 : IGigmatesService
     {
         public string GetData(int value)
@@ -19,18 +22,18 @@ namespace GigmatesWCF
             return string.Format("You entered: {0}", value);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
 
         public string greetings()
         {
@@ -41,6 +44,43 @@ namespace GigmatesWCF
         public string derick()
         {
             return "yow derick";
+        }
+
+        public string Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Signup(SignupUser user)
+        { 
+            string message;
+            int ret;
+
+            //Add data source, initial catalog, password, etc.
+            SqlConnection conn = new SqlConnection("Data Source = ;Initial Catalog = ; Persist Security Info = True; User ID=sa; Password=;Pooling=False");
+            conn.Open();
+            SqlCommand sqlCommand = new SqlCommand("RegisterPerson", conn);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            sqlCommand.Parameters.AddWithValue("@rate", user.Rate);
+            sqlCommand.Parameters.AddWithValue("@location", user.Location);
+            sqlCommand.Parameters.AddWithValue("@username", user.Username);
+            sqlCommand.Parameters.AddWithValue("@password", user.Password);
+            sqlCommand.Parameters.AddWithValue("@bio", user.Biography);
+            sqlCommand.Parameters.AddWithValue("@personType", user.PersonType);
+            sqlCommand.Parameters.AddWithValue("@age", user.Age);
+
+            ret = sqlCommand.ExecuteNonQuery();
+            if (ret == 1)
+            {
+                message = "Sign up successful";
+            }
+            else
+            {
+                message = "Sign up failed";
+            }
+
+            return message;
         }
     }
 
